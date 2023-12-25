02cad1cc3f910ff153fe3162b9f4bbb855af2289
thakuratul2 <pratapsinghatul111@gmail.com>
2023-12-25 19:38:57 +0530

    Final Update

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { NavLink } from "react-router-dom";
+
+
+
+const Navbar = () => {
+  return (
+    <header className='header'>
+      <NavLink to='/'>
+        
+      </NavLink>
+      <nav className='flex text-lg gap-7 font-medium'>
+      <NavLink to='/' className={({ isActive }) => isActive ? "text-blue-900" : "text-black" }>
+          Home
+        </NavLink>
+        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-900" : "text-black" }>
+          About
+        </NavLink>
+        <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-900" : "text-black"}>
+          Projects
+        </NavLink>
+        <NavLink to='/contact' className={({ isActive }) => isActive ? "text-blue-900" : "text-black"}>
+          Contact Me
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ src/constants/index.js
@@ -0,0 +1,145 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    css,
+    estate,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    nodejs,
+    pricewise,
+    react,
+    
+   
+    snapgram,
+    summiz,
+    
+    threads,
+    
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+   
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+  
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+
+];
+
+export const experiences = [
+    {
+        title: "Website Developer",
+        company_name: "Flymax Pvt. Ltd.",
+        date: "May 2022 - July 2022",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Backend Developer",
+        company_name: "Allied Web Design & Software",
+        
+        date: "August 2022 - October 2022",
+        points: [
+            "Developed end-to-end solutions for multiple projects using PHP, MySQL, and Laravel technologies.",
+            "Built high-impact features, including subscription models and Admin Panels.",
+            "Contributing to Allied Company's growth by assisting in the development of scalable solutions for millions.",
+        ],
+    },
+    {
+        title: "WordPress Developer",
+        company_name: "EduClouds Pvt. Ltd.",
+       
+        date: "January 2023 - March 2023",
+        points: [
+            "Implemented a multilingual feature enabling website users to select from various languages for an enhanced user experience.",
+"Successfully developed multiple websites using WordPress technology.",
+"Revamped two main screens & fixed bugs.",
+        ],
+    },
+    {
+        title: "Full stack Developer",
+        company_name: "Sirpi Products & Services Pvt. Ltd.",
+        
+        date: "July 2023 - November 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+   
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Swatipa28',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/swati-paliwal/',
+    }
+];
+
+export const projects = [
+    {
+        
+        name: 'MIET BR APP ADMIN',
+        description: 'This Admin panel website utilizes to built with Php and MySql, allowing for easy CRUD operations on user profiles, notices and admin panel. The user interface is designed for efficient student management and includes a complete process. The admin panel, accessible for CRUD on users and add teachers, includes a chart for statistics display.',
+        link: 'https://github.com/thakuratul2/MIETAPP_ADMIN',
+    },
+    {
+       
+        name: 'Early Brain Magic Website',
+        description: 'This solution providing website built with Html, Css, Javascript. It displayed on a user-friendly interface designed using Bootstrap and styled-components for a seamless experience.',
+        link: 'https://github.com/thakuratul2/BUYBusiness-Internship-',
+    },
+    {
+        name: 'Gurukul Website',
+        description: 'I ve made fully responsive gurkul website which content all the activity performend like events, publish notices.',
+        link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
+    },
+    
+];
