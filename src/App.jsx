d4421b923353d024d4373ca0576954b5502bb8d6
thakuratul2 <pratapsinghatul111@gmail.com>
2023-12-28 19:02:17 +0530

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ const App = () => {
               <>
                 <Routes>
                   <Route path='/about' element={<About />} />
+                  
                   <Route path='/projects' element={<Projects />} />
                   <Route path='/contact' element={<Contact />} />
                 </Routes>
