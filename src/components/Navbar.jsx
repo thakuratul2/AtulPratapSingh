a745995b506316d7327ff3b143d831425f7b8dee
thakuratul2 <pratapsinghatul111@gmail.com>
2024-01-24 18:28:38 +0530

    Fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,6 +10,7 @@ const Navbar = () => {
         
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium'>
+      
       <NavLink to='/' className={({ isActive }) => isActive ? "text-blue-900" : "text-black" }>
           Home
         </NavLink>
