81158fb8a04e2cc24b8896094065af2457ef995c
thakuratul2 <pratapsinghatul111@gmail.com>
2024-01-25 19:49:43 +0530

    Fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,6 @@ const Navbar = () => {
       <NavLink to='/' className={({ isActive }) => isActive ? "text-blue-900" : "text-black" }>
           Home
         </NavLink>
-        
         <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-900" : "text-black" }>
           About
         </NavLink>
