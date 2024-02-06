9fa0ddad9a24dc3a353a9d464cecb89fb032eaa7
thakuratul2 <pratapsinghatul111@gmail.com>
2024-02-06 20:34:57 +0530

    Fix

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 const CTA = () => {
   return (
     <section className='cta'>
+    
       <p className='cta-text'>
       
         Have a project in mind? <br className='sm:block hidden' />
