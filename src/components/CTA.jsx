e6ae0d6d06172b96ebf6b50902aa9093d09c6f13
thakuratul2 <pratapsinghatul111@gmail.com>
2024-02-09 18:06:08 +0530

    Fix

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -9,6 +9,7 @@ const CTA = () => {
         Have a project in mind? <br className='sm:block hidden' />
         
         Let’s build something together!
+        
       </p>
       
       <Link to='/contact' className='btn'>
