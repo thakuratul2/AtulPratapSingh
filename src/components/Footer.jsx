7c29db898fbdb678545f740cb4aa242fbcea2afd
thakuratul2 <pratapsinghatul111@gmail.com>
2024-02-21 22:22:03 +0530

    Fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ import { socialLinks } from "../constants";
 const Footer = () => {
   return (
     <footer className='footer font-poppins'>
+    
       <hr className='border-slate-200' />
 
       <div className='footer-container'>
