3f331376d1f916bc1cdf74907efea7e04c355a38
thakuratul2 <pratapsinghatul111@gmail.com>
2024-02-26 22:21:14 +0530

    Update

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -4,6 +4,7 @@ import { arrow } from "../assets/icons";
 
 const HomeInfo = ({ currentStage }) => {
   if (currentStage === 1)
+  
     return (
       <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
         Hi, I'm
