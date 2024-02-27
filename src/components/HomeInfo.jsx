e162fbb46a07794a9b7f181152a0a83b23a37213
thakuratul2 <pratapsinghatul111@gmail.com>
2024-02-27 22:04:02 +0530

    Update

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -9,6 +9,7 @@ const HomeInfo = ({ currentStage }) => {
       <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
         Hi, I'm
         <span className='font-semibold mx-2 text-white'>Swati Paliwal</span>
+        
         👋
         <br />
         A Flutter Developer & Final Year Student
