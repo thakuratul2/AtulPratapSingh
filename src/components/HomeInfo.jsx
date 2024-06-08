c2f57e4af1694e654ed918e273bad41e895e18fa
thakuratul2 <pratapsinghatul111@gmail.com>
2024-06-08 16:15:00 +0530

    Push test

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -54,6 +54,7 @@ const HomeInfo = ({ currentStage }) => {
 
         <Link to='/projects' className='neo-brutalism-white neo-btn'>
           Visit my portfolio
+          
           <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
         </Link>
       </div>
